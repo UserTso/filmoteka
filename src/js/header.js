0f4d526903a43b6286e8f9af3d4564d1056ca838
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-09-06 18:00:40 +0300

    fix bugs

--- src/js/header.js
+++ src/js/header.js
@@ -10,38 +10,31 @@ const notFound = document.querySelector('.not-found');
 searchForm.addEventListener('submit', searchFilm);
 
 async function searchFilm(event) {
+  event.preventDefault();
 
-    event.preventDefault();
+  // const seartchResult = event.currentTarget.input.value;
+  unsplashAPI.searchQuery = event.currentTarget.input.value
+    .trim()
+    .toLowerCase();
 
-    // const seartchResult = event.currentTarget.input.value;
-    unsplashAPI.searchQuery = event.currentTarget.input.value.trim().toLowerCase();
-
-    // console.log(unsplashAPI.searchQuery);
-    if (unsplashAPI.searchQuery === "") {
-        notFound.style.display = 'none';
-        empty.style.display = 'block';
-        return
+  // console.log(unsplashAPI.searchQuery);
+  if (unsplashAPI.searchQuery === '') {
+    notFound.style.display = 'none';
+    empty.style.display = 'block';
+    return;
+  }
+  try {
+    const { results } = await unsplashAPI.searchMovies();
+    if (results.length === 0) {
+      empty.style.display = 'none';
+      notFound.style.display = 'block';
+      return;
     }
-    try {
 
-        const { results } = await unsplashAPI.searchMovies();
-        if (results.length === 0) {
-            empty.style.display = 'none';
-            notFound.style.display = 'block';
-            return
-        }
-
-        // console.log(results);
-        notFound.style.display = 'none';
-
-        empty.style.display = 'none';
-        renderGalleryItems(results);
-
-        // console.log(results);
-    } catch (error) {
-        console.log(error.message);
-    }
     // console.log(results);
+    notFound.style.display = 'none';
+
+    empty.style.display = 'none';
     renderGalleryItems(results);
 
     // console.log(results);
@@ -49,4 +42,3 @@ async function searchFilm(event) {
     console.log(error.message);
   }
 }
-
